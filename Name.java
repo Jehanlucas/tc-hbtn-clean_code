6fea380cb9e4ea730b41690d2a5ed3089e77bf71
Jehanlucas <jehan@ciandt.com>
2022-07-18 12:58:17 -0300

    Commit inicial

---
+++ Name.java
@@ -0,0 +1,24 @@
+public class Name {
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(Name.User user){};
+
+
+    public List<Account> AccountList;
+
+
+    public void deleteUser(Name.User user){};
+
+
+    public URL url;
+
+
+    public class TableRepresent {};
+    public void saveCar(){};
+
+
+    public saveUser(){}; saveAccount(){}; saveContract(){};
+
+
+    public int daysInYear = 360;
+    public int hoursInWeek = 168;
+}
